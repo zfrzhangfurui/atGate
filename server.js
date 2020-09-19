487ad62b40fc2a036d8ea6bf9db7523859b365cd
Furui Zhang <zfrzhangfurui@gmail.com>
2020-09-19 17:42:44 +0800

    change port

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(errorHandler);
 
 // })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
